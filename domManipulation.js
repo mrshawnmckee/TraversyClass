81af1fb2c33401bba446645b4aa659ccea94d148
Shawn McKee <shawnroosemckee@gmail.com>
2023-09-22 13:04:44 -0400

    more DOM Manipulation

---
+++ domManipulation.js
@@ -0,0 +1,106 @@
+// const para = document.getElementById('graph')
+
+
+// para.innerText= "this is also a paragraph"
+
+// document.getElementById('graph').innerText = "This is a sentence"
+
+// //Change ID title
+// // document.getElementById('graph').title = 'new-id';
+
+
+//Create a variable for use
+const title = document.getElementById('pageTitle')
+
+// Change content  ##Must be in quotes#
+// title.innerHTML = '<h4>Hello Again</h4>'
+
+// Change styles  ## Must use camelCase, no "-" allowed
+// title.style.color = "blue";
+// title.style.backgroundColor = "yellow"
+
+
+// document.querySelector, can use for ('h1'), or a class('.para')
+
+// Choose second item in a list
+// document.querySelector('li:nth-child(2').innerText
+
+
+
+// //  ###Looking at chile/parent/sibling elements
+// let output; 
+
+
+// // Parent elements to access child elements
+// const parent = document.querySelector('.parent');
+
+// output = parent.children[1].innerText = "Child Two"
+// output = parent.children[1].style = "color: blue";
+
+
+// // child elements to access parent elements
+// const child = document.querySelector('.child');
+// output = child.parentElement;
+
+// child.parentElement.style.border = '1px solid black';
+
+
+// // Sibling elements
+
+// const secondItem = document.querySelector('.child:nth-child(2')
+// output = secondItem  //Will access child two
+// output = secondItem.nextElementSibling  //access child three; there is also previousElementSibling
+
+// // DOM node relationship will work with all nodes, not just element nodes
+
+
+// console.log(output)
+
+
+
+
+// // Creating elements
+
+// const div = document.createElement('div'); //this puts it in memory, not placed in page
+// div.className = 'myNewElement'; 
+// const text = document.createTextNode('Hello World');
+// div.appendChild(text)  
+
+// document.body.appendChild(div)
+
+
+// // Creating the function:
+
+// function createListItem(item){
+//     const li = document.createElement('li')
+
+//     li.innerHTML = `${item}`
+
+//     document.querySelector('.listed').appendChild(li)
+// }
+
+// createListItem('eggs')
+// createListItem("radish")
+
+
+// Replace first item 
+
+function replaceFirstItem(){
+    const firstItem = document.querySelector('li:first-child');
+
+    const li = document.createElement('li');
+    li.textContent = "Replaced First item"
+
+    firstItem.replaceWith(li);
+}
+
+replaceFirstItem()
+
+
+function replaceAllItems() {
+    const lis = document.querySelectorAll('li');
+    lis.forEach((item, index) => item.outerHTML = index === 1 ? '<li>SecondItem</li>'  :'<li>replaced</li>'
+)
+}
+
+replaceAllItems()
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('h1').innerText = "hello world"
+// document.querySelector('h1').innerText = "hello world"
 
 // // Challenge one, change first letter of word to uppercase without loop
 // const myString = 'developer'
@@ -149,71 +149,73 @@ document.querySelector('h1').innerText = "hello world"
 // console.log(total)
 
 
-// Array method Challenge 1, create an array called young people and store only name and email of people 25 and under
-const people = [
-    {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john@gmail.com',
-      phone: '111-111-1111',
-      age: 30,
-    },
-    {
-      firstName: 'Jane',
-      lastName: 'Poe',
-      email: 'jane@gmail.com',
-      phone: '222-222-2222',
-      age: 25,
-    },
-    {
-      firstName: 'Bob',
-      lastName: 'Foe',
-      email: 'bob@gmail.com',
-      phone: '333-333-3333',
-      age: 45,
-    },
-    {
-      firstName: 'Sara',
-      lastName: 'Soe',
-      email: 'Sara@gmail.com',
-      phone: '444-444-4444',
-      age: 19,
-    },
-    {
-      firstName: 'Jose',
-      lastName: 'Koe',
-      email: 'jose@gmail.com',
-      phone: '555-555-5555',
-      age: 23,
-    },
-  ];
+// // Array method Challenge 1, create an array called young people and store only name and email of people 25 and under
+// const people = [
+//     {
+//       firstName: 'John',
+//       lastName: 'Doe',
+//       email: 'john@gmail.com',
+//       phone: '111-111-1111',
+//       age: 30,
+//     },
+//     {
+//       firstName: 'Jane',
+//       lastName: 'Poe',
+//       email: 'jane@gmail.com',
+//       phone: '222-222-2222',
+//       age: 25,
+//     },
+//     {
+//       firstName: 'Bob',
+//       lastName: 'Foe',
+//       email: 'bob@gmail.com',
+//       phone: '333-333-3333',
+//       age: 45,
+//     },
+//     {
+//       firstName: 'Sara',
+//       lastName: 'Soe',
+//       email: 'Sara@gmail.com',
+//       phone: '444-444-4444',
+//       age: 19,
+//     },
+//     {
+//       firstName: 'Jose',
+//       lastName: 'Koe',
+//       email: 'jose@gmail.com',
+//       phone: '555-555-5555',
+//       age: 23,
+//     },
+//   ];
 
-//   remember, in the object key:value and extra parenthesis
-const youngPeople = people
-    .filter((person) => person.age <= 25)
-    .map((person) => ({
-        name: person.firstName + ' ' + person.lastName,
-        email: person.email,
-}))
-
-
-console.log(youngPeople)
+// //   remember, in the object key:value and extra parenthesis
+// const youngPeople = people
+//     .filter((person) => person.age <= 25)
+//     .map((person) => ({
+//         name: person.firstName + ' ' + person.lastName,
+//         email: person.email,
+// }))
 
 
-// Array Method challenge 2; add all og the posisitve numbers in the array with array methods
-const numbers = [2, -30, 50, 20, -12, -9, 7];
-
-const positiveSum = numbers
-    .filter((num) => num > 0)
-    .reduce((acc, cur) => acc + cur, 0)
-
-console.log(positiveSum)
-
-// Array Challenge 3; create a new array called Capitalized words that has the first letter of each word capitalized
-
-const words = ['coder', 'programmer', 'developer'];
-
-const capitalizedWords = words.map( word => word[0].toUpperCase() + word.slice(1))
+// console.log(youngPeople)
 
 
-console.log(capitalizedWords)
+// // Array Method challenge 2; add all og the posisitve numbers in the array with array methods
+// const numbers = [2, -30, 50, 20, -12, -9, 7];
+
+// const positiveSum = numbers
+//     .filter((num) => num > 0)
+//     .reduce((acc, cur) => acc + cur, 0)
+
+// console.log(positiveSum)
+
+// // Array Challenge 3; create a new array called Capitalized words that has the first letter of each word capitalized
+
+// const words = ['coder', 'programmer', 'developer'];
+
+// const capitalizedWords = words.map( word => word[0].toUpperCase() + word.slice(1))
+
+
+// console.log(capitalizedWords)
+
+
