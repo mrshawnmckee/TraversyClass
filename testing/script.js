37a5163b7c293680372ab9322b34090ac465cfbd
Shawn McKee <shawnroosemckee@gmail.com>
2023-10-03 21:19:46 -0400

    chuck Norris joke app

---
+++ testing/script.js
@@ -0,0 +1,47 @@
+// // Set interval, change colors
+
+
+// let intervalID;
+
+// function startChange() {
+//     if(!intervalID) {
+//         intervalID = setInterval(changeRandomColor, 1000)
+//     }
+// }
+
+// // // This will toggle between black and white
+// // function changeColor(){
+// //     if (document.body.style.backgroundColor !== "black") {
+// //         document.body.style.backgroundColor = "black"
+// //         document.body.style.color = "white"
+// //     } else {
+// //         document.body.style.backgroundColor = "white"
+// //         document.body.style.color = "black"
+// //     }
+// // }
+
+// // This will bring a random color
+
+// function changeRandomColor() {
+//     const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+//     document.body.style.backgroundColor = `#${randomColor}`
+// }
+
+// function stopChange() {
+//     clearInterval(intervalID);
+// }
+
+// document.getElementById('stop').addEventListener('click', stopChange)
+// document.getElementById('start').addEventListener('click', startChange)
+
+
+
+
+
+
+
+
+
+
+
+
