da992132eff0096901b96ace5b0b2ba75d143871
Shawn McKee <shawnroosemckee@gmail.com>
2023-10-05 14:15:21 -0400

    random user prmini project

--- randomUserFetch/script.js
+++ randomUserFetch/script.js
@@ -1,12 +1,60 @@
-const generateBtn = document.getElementById('generate')
-
-
-function generateRandomUser() {
-    fetch('https://randomuser.me/api/')
-      .then((response) => response.json())
-      .then((data) => {
-        // document.getElementById('space-y-3').firstChild.firstChild.textContent = data.name.first
-      })
+function fetchUser() {
+  showSpinner();
+  fetch('https://randomuser.me/api')
+   .then((res) => res.json())
+   .then((data) => {
+    hideSpinner();
+      displayUser(data.results[0]);
+   });
 }
 
-generateBtn.addEventListener('click', generateRandomUser)
+function displayUser(user){
+   const userDisplay = document.querySelector('#user')
+
+
+  if(user.gender === 'female') {
+    document.body.style.backgroundColor = "rebeccapurple"
+  } else {
+    document.body.style.backgroundColor = "steelblue"
+  }
+
+  userDisplay.innerHTML = `
+      <div class="flex justify-between">
+      <div class="flex">
+        <img
+          class="w-48 h-48 rounded-full mr-8"
+          src="${user.picture.large}"
+        />
+        <div class="space-y-3">
+          <p class="text-xl">
+            <span class="font-bold">Name: </span>${user.name.first} ${user.name.last}
+          </p>
+          <p class="text-xl">
+            <span class="font-bold">Email: </span> ${user.email}
+          </p>
+          <p class="text-xl">
+            <span class="font-bold">Phone: </span> ${user.phone}
+          </p>
+          <p class="text-xl">
+            <span class="font-bold">Location: </span> ${user.location.city} ${user.location.state}
+          </p>
+          <p class="text-xl"><span class="font-bold">Age: </span> ${user.dob.age}</p>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+function showSpinner() {
+  document.querySelector('.spinner').style.display = 'block';   //To show it 
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').style.display = 'none';   //To hide it 
+}
+
+
+document.querySelector('#generate').addEventListener('click', fetchUser)
+
+fetchUser();
+
